4390c406ae9eafb0d39b45c321a78aa33d2b0aa3
Lindsay Watt <lindsayrgwatt@gmail.com>
2021-07-18 09:49:29 -0700

    Add files via upload
    Added core code + pyportal case

---
+++ code.py
@@ -0,0 +1,76 @@
+import adafruit_sdcard
+import board
+import busio
+import digitalio
+import storage
+
+import adafruit_imageload
+import displayio
+import gc
+import os
+import random
+import time
+
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.SD_CS)
+
+sdcard = adafruit_sdcard.SDCard(spi, cs)
+vfs = storage.VfsFat(sdcard)
+storage.mount(vfs, "/sd")
+
+# All files will be in a folder called 'topics'. Each subfolder will contain a theme
+path = "/sd/topics"
+
+raw_topics = os.listdir(path)
+
+topics = {}
+for topic in raw_topics:
+    if topic[0] != '.':
+        topics[topic] = []
+
+for topic in topics:
+    raw_files = os.listdir(path + "/" + topic)
+    for entry in raw_files:
+        if entry[-3:] == 'bmp' and entry[0] != '.':
+            topics[topic].append(entry)
+
+display = board.DISPLAY
+display.rotation=270
+display.auto_brightness = False
+
+group = displayio.Group(max_size=1)
+
+#counter = 0
+while True:
+    base_path = "/sd/topics/"
+    directory = random.choice(list(topics.keys()))
+    filename = random.choice(topics[directory])
+
+    full_path = base_path + directory + "/" + filename
+
+    image, palette = adafruit_imageload.load(full_path, bitmap=displayio.Bitmap, palette=displayio.Palette)
+    tile_grid = displayio.TileGrid(image, pixel_shader=palette)
+
+    group.append(tile_grid)
+
+    board.DISPLAY.show(group)
+
+    # Fade in
+    for x in range(51):
+        display.brightness = x * 0.02
+        time.sleep(1)
+
+    time.sleep(180)
+
+    # Fade out
+    for x in range(51):
+        display.brightness = 1 - (x * 0.02)
+        time.sleep(1)
+
+    group.pop()
+    del tile_grid
+    del image
+    del palette
+
+    gc.collect()
