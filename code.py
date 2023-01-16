f132941df288290c61cbb9e897ff4eb8059d2d5a
Lindsay Watt <lindsayrgwatt@gmail.com>
2023-01-15 16:05:01 -0800

    Enable touch on/off
    You can now touch the screen to toggle it on/off. Required changing how images are displayed in endless loop.

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import adafruit_sdcard
+import adafruit_touchscreen
 import board
 import busio
 import digitalio
@@ -6,11 +7,11 @@ import storage
 
 import adafruit_imageload
 import displayio
+import neopixel
 import gc
 import os
 import random
 import time
-
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.SD_CS)
@@ -39,39 +40,70 @@ display = board.DISPLAY
 display.rotation=270
 display.auto_brightness = False
 
+screen_width = 240
+screen_height = 320
+ts = adafruit_touchscreen.Touchscreen(board.TOUCH_XL, board.TOUCH_XR,
+                                      board.TOUCH_YD, board.TOUCH_YU,
+                                      calibration=((5200, 59000), (5800, 57000)),
+                                      size=(screen_width, screen_height))
+
+visible = True
+
 group = displayio.Group(max_size=1)
 
-#counter = 0
+base_path = "/sd/topics/"
+print('starting up')
+
+max_counter = 100000 # By experimentation
+counter = max_counter
+
+
 while True:
-    base_path = "/sd/topics/"
-    directory = random.choice(list(topics.keys()))
-    filename = random.choice(topics[directory])
+    if counter == max_counter:    
+        directory = random.choice(list(topics.keys()))
+        filename = random.choice(topics[directory])
 
-    full_path = base_path + directory + "/" + filename
+        full_path = base_path + directory + "/" + filename
 
-    image, palette = adafruit_imageload.load(full_path, bitmap=displayio.Bitmap, palette=displayio.Palette)
-    tile_grid = displayio.TileGrid(image, pixel_shader=palette)
+        image, palette = adafruit_imageload.load(full_path, bitmap=displayio.Bitmap, palette=displayio.Palette)
+        tile_grid = displayio.TileGrid(image, pixel_shader=palette)
 
-    group.append(tile_grid)
+        group.append(tile_grid)
 
-    board.DISPLAY.show(group)
+        board.DISPLAY.show(group)
 
-    # Fade in
-    for x in range(10):
-        display.brightness = (x + 1) * 0.1
-        time.sleep(6)
-
-    # Show each image for 3 minutes
-    time.sleep(180)
-
+        # Fade in
+        for x in range(10):
+            display.brightness = (x + 1) * 0.1
+            time.sleep(1)
+        
+        counter = 0
+    
+    if visible:
+        counter += 1
+    
+    touch = ts.touch_point # STRANGE: seems to be causing an audible whine in PyPortal board
+    
+    if touch:
+        time.sleep(0.5) # Will get multiple touches so temporarily block so only get one
+        if visible:
+            visible = False
+            display.brightness = 0
+        else:
+            visible = True
+            display.brightness = 1
+    
+    gc.collect() # Even stranger: adding this stopped the audible whine
+    
     # Fade out
-    for x in range(10):
-        display.brightness = 1 - ((x + 1) * 0.1)
-        time.sleep(6)
+    if counter == max_counter:
+        for x in range(10):
+            display.brightness = 1 - ((x + 1) * 0.1)
+            time.sleep(1)
 
-    group.pop()
-    del tile_grid
-    del image
-    del palette
+        group.pop()
+        del tile_grid
+        del image
+        del palette
 
-    gc.collect()
+        gc.collect()
